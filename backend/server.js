79d37cf231e1bd415274afd6290fd4670c352c5d
shirish-raj-gupta <codersrg@gmail.com>
2025-04-30 13:50:11 +0530

    backend start

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
+//App config
+const app = express()
+const port = process.env.PORT || 5000
+connectDB()
+connectCloudinary()
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api end points
+app.get('/', (req, res) => res.status(200).send('api working'))
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
