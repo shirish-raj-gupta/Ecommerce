708a35a112636e2b29ccd0401800d4b5addcfa00
shirish-raj-gupta <codersrg@gmail.com>
2025-04-30 12:51:48 +0530

    frontend complete

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Title from '../components/Title'
+import {assets} from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+    <div className='text-2xl text-center pt-8 border-t'>
+      <Title text1={'About'} text2={'Us'}/>
+    </div>
+    <div className='my-10 flex flex-col md:flex-row gap-16'>
+      <img src={assets.about_img} alt="about" className='w-full md:max-w-[450px]'/>
+      <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+      <p>about us</p>
+      <p>good to go</p>
+      <b className='text-gray-800'>Our Mission</b>
+      <p>going good</p>
+      </div>
+     
+
+    </div>
+    <div className='text-xl py-4 '>
+        <Title text1={'Why'} text2={'Choose us'}/>
+      </div>
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Yeah We are good</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>Yeah We are good</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptiional Customer Service:</b>
+          <p className='text-gray-600'>Yeah We are good</p>
+        </div>
+
+      </div>
+      <NewsletterBox/>
+    </div>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [currentState, setCurrentState] = useState("Login");
+  const onSubmitHandler = async(e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 text-gray-800 gap-4'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+         <p className='prata-regular text-3xl'>{currentState}</p>
+         <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
+      </div>
+      {currentState==='Login'? '': <input type='Name' placeholder='First Name' className='border border-gray-800 rounded py-2 px-3 w-full'required/>}
+      <input type='email' placeholder='Email' className='border border-gray-800 rounded py-2 px-3 w-full'required/>
+      <input type='password' placeholder='Pasword' className='border border-gray-800 rounded py-2 px-3 w-full' required/>
+      <div className='w-full flex justify-between text-sm mt-[-8px]'>
+        <p className='cursor-pointer'>Forgot your password?</p>
+        {
+          currentState==='Login' 
+          ? <p onClick={()=>setCurrentState('Sign Up')} className='cursor-pointer'>Create an account</p> 
+          : <p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
+        }
+      </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState==='Login'? 'Sign In' : 'Sign Up'}</button>
+    </form>
   )
 }
 
